220e62c9309fa383bea3b7d31a56ecf11ee3f5f2
Filippo Casarin <casarin.filippo17@gmail.com>
2022-11-28 14:37:34 +0100

    Linter

--- preprocess.py
+++ preprocess.py
@@ -5,23 +5,21 @@ import re
 MARGIN = 100 - 40 - 8 + 7
 
 
-def mkhash(dat):
+def mkhash(dat: list[str]):
     conc = ""
     for i in dat:
-        conc += re.sub('\s*', '', i) + "\n"
-        yield hashlib.md5(conc.encode('utf8')).hexdigest()[:2]
+        conc += re.sub(r"\s*", "", i) + "\n"
+        yield hashlib.md5(conc.encode("utf8")).hexdigest()[:2]
 
-for path, dirs, files in os.walk('./_src'):
+for path, dirs, files in os.walk("./_src"):
     for f in files:
-        if f[f.rfind('.')+1:] in ['cpp', 'hpp', 'java', 'sh']:
+        if f[f.rfind('.')+1:] in ["cpp", "hpp", "java", "sh"]:
             p = os.path.join(path, f)
 
             with open(p) as file:
                 text = file.read()
-            if not f.endswith('.sh'):
-                text = re.sub(r'\n?[ \t]*(?<!/)//[^/].+', '', text)
-            #text = re.sub(r'\n?[ \t]*//[^/].+', '', text)
-            #dat = [ line for line in text.splitlines() if not line.startswith('// vim: ') and not line.startswith('# vim: ') ]
+            if not f.endswith(".sh"):
+                text = re.sub(r"\n?[ \t]*(?<!/)//[^/].+", "", text)
             dat = text.splitlines()
             out = open(p, 'w')
 
@@ -37,13 +35,13 @@ for path, dirs, files in os.walk('./_src'):
                     add -= 1
                 s = ' '*add + s
                 if(len(s) > MARGIN):
-                    print>>out, s
+                    print(s, file=out)
                     warning = True
                     last = True
                     if len(s) > MARGIN+4:
                         error = True
-                        print (len(s), MARGIN)
-                        print (repr(s))
+                        print(len(s), MARGIN)
+                        print(repr(s))
                 else:
                     if len(s) < MARGIN:
                         s = s+' '
